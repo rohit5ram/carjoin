efb8f63ce2ede5ff936e82b689c3e71f264b4f76
vishnu.ganta22 <vishnu.ganta22@gmail.com>
2016-11-26 22:00:17 +0530

    -added support for current user location
    Signed-off-by: vishnu.ganta22 <vishnu.ganta22@gmail.com>

--- app/src/main/java/com/pr/carjoin/Util.java
+++ app/src/main/java/com/pr/carjoin/Util.java
@@ -1,12 +1,24 @@
 package com.pr.carjoin;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
+import android.telephony.TelephonyManager;
+import android.telephony.gsm.GsmCellLocation;
+import android.text.TextUtils;
 import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by rohit on 31/5/15.
  */
 public class Util {
     public static final String TAG = "com.letzpool";
+
     public static void logException(Exception e, String logLabel) {
         Log.e(TAG, logLabel + " [[[ " + e.getMessage() + " ]]]");
         for (StackTraceElement el : e.getStackTrace()) {
@@ -14,7 +26,68 @@ public class Util {
         }
     }
 
-    public static boolean isUserAlreadySignedIn(){
+    public static boolean isUserAlreadySignedIn() {
         return true;
+    }
+
+    public static String getRequestBodyForLocationRequest(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo.isConnected()) {
+            switch (networkInfo.getType()) {
+                case ConnectivityManager.TYPE_WIFI:
+                    return getRequestBodyForLocationWithNetworkWifi(context);
+                case ConnectivityManager.TYPE_MOBILE:
+                    return getRequestBodyForLocationWithNetworkMobile(context);
+                case ConnectivityManager.TYPE_ETHERNET:
+                    return "";
+            }
+        }
+        return "";
+    }
+
+    private static String getRequestBodyForLocationWithNetworkWifi(Context context) {
+        JSONObject jsonObject = new JSONObject();
+        JSONObject wifiJsonObject = new JSONObject();
+        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        try {
+            wifiJsonObject.put("macAddress", wifiManager.getConnectionInfo().getBSSID());
+            jsonObject.put("wifiAccessPoints", wifiJsonObject);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return jsonObject.toString();
+    }
+
+    public static String getRequestBodyForLocationWithNetworkMobile(Context context) {
+        int mcc = -1;
+        int mnc = -1;
+        int cid = -1;
+        int lac = -1;
+        JSONObject jsonObject = new JSONObject();
+        JSONArray jsonArray = new JSONArray();
+        JSONObject mobileJsonObject = new JSONObject();
+        TelephonyManager manager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        String networkOperator = manager.getNetworkOperator();
+        if (!TextUtils.isEmpty(networkOperator)) {
+            mcc = Integer.parseInt(networkOperator.substring(0, 3));
+            mnc = Integer.parseInt(networkOperator.substring(3));
+        }
+        GsmCellLocation cellLocation = (GsmCellLocation) manager.getCellLocation();
+        if(cellLocation != null){
+            cid = cellLocation.getCid() & 0xffff;  // GSM cell id
+            lac = cellLocation.getLac() & 0xffff;  // GSM Location Area Code
+        }
+        try {
+            mobileJsonObject.put("cellId", cid);
+            mobileJsonObject.put("locationAreaCode", lac);
+            mobileJsonObject.put("mobileCountryCode", mcc);
+            mobileJsonObject.put("mobileNetworkCode", mnc);
+            jsonArray.put(mobileJsonObject);
+            jsonObject.put("cellTowers", jsonArray);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jsonObject.toString();
     }
 }
--- app/src/main/java/com/pr/carjoin/activities/MainActivity.java
+++ app/src/main/java/com/pr/carjoin/activities/MainActivity.java
@@ -1,14 +1,22 @@
 package com.pr.carjoin.activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,29 +24,77 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.pr.carjoin.PermissionUtils;
 import com.pr.carjoin.R;
+import com.pr.carjoin.Util;
 import com.pr.carjoin.chat.ChatMainActivity;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
-    private FirebaseAuth firebaseAuth;
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback,
+        GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, LocationListener {
+    private static final String LOG_LABEL = "activities.MainActivity";
     private FirebaseUser firebaseUser;
+    private FloatingActionButton startTripFab;
+    private GoogleMap mMap;
+    private LocationRequest locationRequest;
+    private GoogleApiClient mGoogleApiClient;
+    private Location location;
+    private Marker pickUpLocationMarker;
+
+    /**
+     * Request code for location permission request.
+     *
+     * @see #onRequestPermissionsResult(int, String[], int[])
+     */
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    /**
+     * Flag indicating whether a requested permission has been denied after returning in
+     * {@link #onRequestPermissionsResult(int, String[], int[])}.
+     */
+    private boolean mPermissionDenied = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        firebaseAuth = FirebaseAuth.getInstance();
+        buildGoogleApiClient();
+        createLocationRequest();
+        mGoogleApiClient.connect();
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
 
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton myLocationFab = (FloatingActionButton) findViewById(R.id.my_location_fab);
+        myLocationFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (location != null) {
+                    LatLng coordinates = new LatLng(location.getLatitude(), location.getLongitude());
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 15));
+                    pickUpLocationMarker.setPosition(coordinates);
+                }
+            }
+        });
+        startTripFab = (FloatingActionButton) findViewById(R.id.start_trip);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -110,7 +166,16 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap = googleMap;
+        pickUpLocationMarker = mMap.addMarker(new MarkerOptions().title("PickUp Location").position(new LatLng(0, 0)).draggable(true));
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+        mMap.setOnCameraMoveListener(new GoogleMap.OnCameraMoveListener() {
+            @Override
+            public void onCameraMove() {
+                pickUpLocationMarker.setPosition(mMap.getCameraPosition().target);
+            }
+        });
+        enableMyLocation();
     }
 
     private void setNavHeader(View headerView) {
@@ -122,5 +187,129 @@ public class MainActivity extends AppCompatActivity
 
         TextView emailView = (TextView) headerView.findViewById(R.id.nav_header_sub_title);
         emailView.setText(firebaseUser.getEmail());
+    }
+
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation();
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true;
+        }
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (mPermissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            mPermissionDenied = false;
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
+    }
+
+    private void createLocationRequest() {
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(6000000);
+        locationRequest.setFastestInterval(300000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,
+                locationRequest, this);
+        if (location != null) {
+            Log.i(Util.TAG, LOG_LABEL + " current location of the device is :: " + location.toString());
+            LatLng coordinates = new LatLng(location.getLatitude(), location.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 15));
+            pickUpLocationMarker.setPosition(coordinates);
+        }
+    }
+
+    private synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */,
+                        this /* OnConnectionFailedListener */)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .build();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i(Util.TAG, LOG_LABEL + " connection to google API is failed");
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.i(Util.TAG, LOG_LABEL + " connected to google API");
+        getLocation();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(Util.TAG, LOG_LABEL + " connection to google API is suspended");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i(Util.TAG, LOG_LABEL + " location of the device is changed");
+        LatLng coordinates = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 15));
+        pickUpLocationMarker.setPosition(coordinates);
     }
 }
